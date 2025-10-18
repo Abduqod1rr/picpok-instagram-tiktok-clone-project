f0a80fa26df069a492d85d36f0fc74aeebca9924
Abduqodir <ttheonepersonn@gmail.com>
2025-10-18 19:33:35 +0500

    .

--- main/views.py
+++ main/views.py
@@ -3,5 +3,7 @@ from django.views.generic import CreateView , ListView ,UpdateView ,DeleteView
     # Create your views here.
 
 def hello1func(request):
+
     return HttpResponse("hello it's the picpok project i'm making docs and readme")
+
 
