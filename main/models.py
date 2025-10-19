ba0bcfda25b9fdb7b022e3dc8912ca544a323993
Abduqodir <ttheonepersonn@gmail.com>
2025-10-19 20:02:31 +0500

    pocs model

--- main/models.py
+++ main/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-
+class Poc(models.Model):
+    title=models.CharField(max_length=20)
+    content=models.FileField(upload_to='/picpokcontent')
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
