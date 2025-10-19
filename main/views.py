f57a375df8a188dfce72a0fc2e74dbf4b3687a69
Abduqodir <ttheonepersonn@gmail.com>
2025-10-19 19:56:28 +0500

    .

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse 
 from django.views.generic import CreateView , ListView ,UpdateView ,DeleteView    
-    # Create your views here.
+    
 
 def hello1func(request):
 
