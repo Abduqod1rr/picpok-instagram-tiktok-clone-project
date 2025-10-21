5ec87f2e5c0ffae1a890f26ccc25dcc33225af1e
Abduqodir <ttheonepersonn@gmail.com>
2025-10-21 19:47:08 +0500

    .

--- main/views.py
+++ main/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, HttpResponse
 from django.views.generic import CreateView , ListView ,UpdateView ,DeleteView    
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from . import forms
 
-class UserRegister(UserCreationForm,CreateView):
-        fields =  ['username', 'password1', 'password2']
+class UserRegister(CreateView):
+        form_class=forms.cutomuserform
         template_name='register.html'
         model=User
         
