01753c0b68fff0860b8b0346e1afdd12c2a6c7d5
Abduqodir <ttheonepersonn@gmail.com>
2025-10-22 18:52:50 +0500

    .

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
      #paths
-    path("register", views.UserRegister.as_view(), name="register"),
-    path("login/", views.UserLogin.as_view(), name="login")
+    path("register/", views.UserRegister.as_view(), name="register"),
+    path("login/", views.UserLogin.as_view(), name="login"),
+    path("create-profile/", views.CreateProfile.as_view(), name="createprofile")
 ]
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from . import forms
 from django.contrib.auth.views import LoginView , LogoutView
 from django.urls import reverse_lazy
+from .models import Poc ,Profile ,Comment
 
 class UserRegister(CreateView):
         form_class=forms.cutomuserform
@@ -15,3 +16,9 @@ class UserRegister(CreateView):
 class UserLogin(LoginView):
         template_name='login.html'
         success_url=reverse_lazy('register')
+
+class CreateProfile(CreateView):
+        model=Profile
+        fields=['bio','picture']
+        template_name='createprofile.html'
+        success_url=reverse_lazy('login')
